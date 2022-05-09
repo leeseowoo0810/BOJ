47636abf4c5bee00cb95120ddffce1f5121dcef5
leeseowoo0810 <po01033171881@gmail.com>
2022-05-10 00:44:48 +0900

    5/13 00:44

---
+++ 수_정렬하기.py
@@ -0,0 +1,7 @@
+a = []
+for _ in range(int(input())):
+    N = int(input())
+    a.append(N)
+a.sort()
+for i in a:
+    print(i)
---
+++ 수_정렬하기2.py
@@ -0,0 +1,7 @@
+a = []
+for _ in range(int(input())):
+    b= int(input())
+    a.append(b)
+a.sort()
+for i in a:
+    print(i)
